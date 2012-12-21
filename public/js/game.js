d73e8feece8fcdb2bc9ea7b95e5086ee6aaf0287
Han Dao <hackerkiba@gmail.com>
2012-12-20 20:09:05 -0500

    update game.js

--- public/js/game.js
+++ public/js/game.js
@@ -1,5 +1,14 @@
 (function() {
-  var menu;
+  var frameRateDraw, menu;
+
+  frameRateDraw = function(p5) {
+    this.p5 = p5;
+    this.p5.fill(0);
+    this.p5.noStroke();
+    this.p5.rect(200, 0, 50, 20);
+    this.p5.fill(255);
+    return this.p5.text("FPS: " + Math.floor(this.p5.__frameRate), 200, 15);
+  };
 
   menu = function(p5) {
     p5.setup = function() {
@@ -7,7 +16,9 @@
       p5.background(0);
       return this.fps = 50;
     };
-    return p5.draw = function() {};
+    return p5.draw = function() {
+      return frameRateDraw(p5);
+    };
   };
 
   $(document).ready(function() {
